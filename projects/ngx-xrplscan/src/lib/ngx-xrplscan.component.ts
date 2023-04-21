12432799359e0543ede7e4895b5686cfe7b64fb3
Soolan <info@soolan.com>
2023-04-21 21:17:12 +0800

    [ch02] create a new library named ngx-xrplscan

---
+++ projects/ngx-xrplscan/src/lib/ngx-xrplscan.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-xrplscan',
+  template: `
+    <p>
+      ngx-xrplscan works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgxXrplscanComponent {
+
+}
---
+++ projects/ngx-xrplscan/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-xrplscan
+ */
+
+export * from './lib/ngx-xrplscan.service';
+export * from './lib/ngx-xrplscan.component';
+export * from './lib/ngx-xrplscan.module';
